3b5d930320e25d25bbdf0ff1803753493517a4c9
lordxnoka <lord.noka@gmail.com>
2015-10-01 19:02:48 +0300

    Add desktop version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(".get-code").click(function(){
+	$(".full.prepage").slideUp();
+	$("body").addClass("on");
+})
+
+$('.scratch').wScratchPad({
+  size        : 50,          // The size of the brush/scratch.
+  bg          : '#cdcdcd',  // Background (image path or hex color).
+  fg          : 'images/scratch-bg.png',  // Foreground (image path or hex color).
+  realtime    : true,       // Calculates percentage in realitime.
+  scratchDown : null,       // Set scratchDown callback.
+  scratchUp   : null,       // Set scratchUp callback.
+  scratchMove : null,       // Set scratcMove callback.
+  cursor      : 'pointer' // Set cursor.
+});
