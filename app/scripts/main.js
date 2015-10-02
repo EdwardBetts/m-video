33f31884aeb57edf14b40f8fd0f5267ac9d28cb6
lordxnoka <lord.noka@gmail.com>
2015-10-02 18:43:34 +0300

    Add hand animation and scratch help (beta)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,19 @@
 $(".get-code").click(function(){
 	$(".full.prepage").slideUp();
 	$("body").addClass("on");
-})
+});
 
-$('.scratch').wScratchPad({
+$(".scratch").wScratchPad({
   size        : 50,          // The size of the brush/scratch.
-  bg          : '#cdcdcd',  // Background (image path or hex color).
-  fg          : 'images/scratch-bg.png',  // Foreground (image path or hex color).
+  bg          : "#cdcdcd",  // Background (image path or hex color).
+  fg          : "images/scratch-bg.png",  // Foreground (image path or hex color).
   realtime    : true,       // Calculates percentage in realitime.
   scratchDown : null,       // Set scratchDown callback.
   scratchUp   : null,       // Set scratchUp callback.
   scratchMove : null,       // Set scratcMove callback.
-  cursor      : 'pointer' // Set cursor.
+  cursor      : "pointer" // Set cursor.
+});
+
+$(".scratch").on("mouseDown", function(){
+  $(".action-help").fadeOut();
 });
